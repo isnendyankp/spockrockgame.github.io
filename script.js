da910aa12f1ef36936a8afd6ab05a0ed924439ce
isnendyan <isnendyankp@gmail.com>
2023-05-06 09:37:47 +0700

    Js. computer selection: Add 'selected' styling & computerChoice

--- script.js
+++ script.js
@@ -52,6 +52,35 @@ function computerRandomChoice() {
   console.log(computerChoice);
 }
 
+// Add 'selected' styling & computerChoice
+function displayComputerChoice() {
+  switch (computerChoice) {
+    case 'rock':
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Rock';
+      break;
+    case 'paper':
+      computerPaper.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Paper';
+      break;
+    case 'scissors':
+      computerScissors.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Scissors';
+      break;
+    case 'lizard':
+      computerLizard.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Lizard';
+      break;
+    case 'spock':
+      computerSpock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Spock';
+      break;
+    default:
+      break;
+  }
+}
+
+
 // Call functions to process turn
 function checkResult() {
   resetSelected();
