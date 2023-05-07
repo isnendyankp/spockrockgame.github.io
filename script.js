dfd5d4df457bc7f8361bc0f73996d52ac624b6db
isnendyan <isnendyankp@gmail.com>
2023-05-07 09:39:01 +0700

    Js. Check Results: add function base won/lose statement

--- script.js
+++ script.js
@@ -87,6 +87,9 @@ function updateScore(playerChoice) {
   console.log(playerChoice, computerChoice);
   if (playerChoice === computerChoice) {
     resultText.textContent = "It's a tie.";
+  } else {
+    const choice = choices[playerChoice];
+    console.log(choice.defeats.indexOf(computerChoice));
   }
 }
 
